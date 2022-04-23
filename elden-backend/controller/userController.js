bd4d7f70727e60305a5195225b44d6b0bd2ded15
LuisRococo <luisrobertopenav@gmail.com>
2022-04-23 13:23:19 -0700

    Se refactorizo el controlador

--- elden-backend/controller/userController.js
+++ elden-backend/controller/userController.js
@@ -1,11 +1,17 @@
 const { User } = require("../src/db/models/userModel");
 const { getSuccessAnswer } = require("../util/util");
 
-async function registerUser(userName, email, password) {
-  const isAdmin = false;
-  const newUser = new User({ userName, email, password, isAdmin });
-  await newUser.save();
-  return getSuccessAnswer();
+async function registerUser(req, res, next) {
+  try {
+    const { userName, email, password } = req.body;
+    const isAdmin = false;
+    const newUser = new User({ userName, email, password, isAdmin });
+
+    await newUser.save();
+    res.json(getSuccessAnswer());
+  } catch (error) {
+    next(error);
+  }
 }
 
 module.exports = { registerUser };
--- elden-backend/routes/userRoute.js
+++ elden-backend/routes/userRoute.js
@@ -2,15 +2,8 @@ const express = require("express");
 const { nextTick } = require("process");
 const { registerUser } = require("../controller/userController");
 const router = express.Router();
+const userController = require("../controller/userController");
 
-router.post("/", async (req, res, next) => {
-  try {
-    const { userName, password, email } = req.body;
-    const asnwer = await registerUser(userName, email, password);
-    res.send(asnwer);
-  } catch (error) {
-    next(error);
-  }
-});
+router.post("/", userController.registerUser);
 
 module.exports = router;
