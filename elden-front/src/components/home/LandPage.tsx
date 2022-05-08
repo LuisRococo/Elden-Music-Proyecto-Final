a84558621ca99e699c5a3fed9d5e567a23aa55c7
LuisRococo <luisrobertopenav@gmail.com>
2022-05-07 20:51:48 -0700

    Added land page

--- elden-front/src/components/home/LandPage.tsx
+++ elden-front/src/components/home/LandPage.tsx
@@ -1,5 +1,29 @@
+import { Container, Grid } from "@mui/material";
+import { Box } from "@mui/system";
 import React from "react";
+import headphoneImg from "../../img/white-headphones.webp";
 
 export default function LandPage() {
-  return <div className=""></div>;
+  return (
+    <div className="land-page">
+      <Container maxWidth="lg" sx={{ height: "100%" }}>
+        <div className="land-page__content">
+          <div className="land-page__info">
+            <h2 className="land-page__title">Get lost into music</h2>
+            <p className="land-page__">Everything you need is here</p>
+          </div>
+          <Box
+            sx={{
+              display: { md: "flex", xs: "none" },
+              justifyContent: "center",
+              alignItems: "center",
+              flex: "1",
+            }}
+          >
+            <img className="land-page__img" src={headphoneImg} alt="" />
+          </Box>
+        </div>
+      </Container>
+    </div>
+  );
 }
--- elden-front/src/pages/HomePage.tsx
+++ elden-front/src/pages/HomePage.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import HomeSongsSection from "../components/home/HomeSongsSection";
 import LandPage from "../components/home/LandPage";
 
 export default function HomePage() {
   return (
     <div>
       <LandPage />
+      <HomeSongsSection />
     </div>
   );
 }
