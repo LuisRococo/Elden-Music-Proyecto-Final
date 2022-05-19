f1f1a4fb131fa08e792fea6e9c0218711e832c71
Luis Roberto <luisroberto@Luiss-MacBook-Pro-2.local>
2022-05-19 12:15:34 -0700

    changed front config to local server

--- elden-front/src/util/other.ts
+++ elden-front/src/util/other.ts
@@ -1,7 +1,7 @@
 import store from "../redux/store";
 
-const URL_DIR = "http://3.218.67.164:10026";
-// const URL_DIR = "http://localhost:8000";
+// const URL_DIR = "http://3.218.67.164:10026";
+const URL_DIR = "http://localhost:8000";
 
 async function toBase64(file) {
     return new Promise((resolve, reject) => {
